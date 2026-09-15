2d88d4f982ab3331f70314e729188a0f1575881b
solde <71665509+soldee@users.noreply.github.com>
2026-09-15 13:18:51 +0200

    configured env variables and db connection

--- docker/pql/schema.sh
+++ docker/pql/schema.sh
@@ -1,12 +1,16 @@
+#!/bin/bash
+# running a bash script (instead of the sql directly) so that we can inject env variables
+
+psql $POSTGRES_DB << EOF
 CREATE EXTENSION vector;
 
 CREATE TABLE IF NOT EXISTS arxiv_meta (
     id bigserial PRIMARY KEY,
     title text NOT NULL,
     abstract text NOT NULL,
-    embedding vector(768) NOT NULL,
+    embedding vector($EMBEDDING_MODEL_DIM) NOT NULL,
     fts tsvector GENERATED ALWAYS AS (to_tsvector('english', title || ' ' || abstract)) STORED
 );
 
 CREATE INDEX fts_idx ON arxiv_meta USING GIN (fts);
-
+EOF
---
+++ src/config.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_ARXIV_TABLE: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+
+    EMBEDDING_MODEL_NAME: str
+    EMBEDDING_MODEL_DIM: int
+
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', extra='ignore')
+
+    @property
+    def DB_URL(self) -> str:
+        return (
+            f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
+            f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
+
+cfg = Settings()
